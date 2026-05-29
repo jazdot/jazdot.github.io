b19dd48f585dba31499b4ce6de33d874229a0387
jazdot <riswanmp6@gmail.com>
2026-05-29 23:55:48 +0530

    fix: set correct base path for github pages root

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This tells Vite to use relative paths for your assets
+  base: '/', // This must be '/' for jazdot.github.io
 })
-
